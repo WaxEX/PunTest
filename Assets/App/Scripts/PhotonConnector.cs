7203be45ded5a7a8ac27cbb8b63385de5464f34b
Wataru Mizutani <mizutani@success-corp.co.jp>
2016-07-13 20:31:03 +0900

    no message

---
+++ Assets/App/Scripts/PhotonConnector.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+// using System.Collections;
+
+public class PhotonConnector : Photon.PunBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Start");
+
+
+		Debug.Log(PhotonNetwork.lobby);
+		Debug.Log(PhotonNetwork.insideLobby);
+
+		PhotonNetwork.ConnectUsingSettings("0.1");
+
+	}
+
+
+
+	public override void OnJoinedLobby()
+	{
+		Debug.Log("OnJoinedLobby");
+
+		Debug.Log(PhotonNetwork.lobby);
+		Debug.Log(PhotonNetwork.insideLobby);
+
+		_loggingLobbyList ();
+	}
+
+	// when AutoJoinLobby is off, this method gets called when PUN finished the connection (instead of OnJoinedLobby())
+	public override void OnConnectedToMaster()
+	{
+		Debug.Log("OnConnectedToMaster");
+
+
+
+		Debug.Log(PhotonNetwork.lobby);
+		Debug.Log(PhotonNetwork.insideLobby);
+
+
+		_loggingLobbyList ();
+
+
+
+
+		TypedLobby targetLobby = new TypedLobby("testLobby", LobbyType.Default); 
+
+		PhotonNetwork.JoinLobby(targetLobby);
+
+
+
+
+
+
+	}
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void OnGUI()
+	{
+		GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString ());
+	}
+		
+
+// for DEBUG
+
+	private void _loggingLobbyList(){
+		var list = "===Lobbies: ";
+		list += PhotonNetwork.LobbyStatistics.Count+"===\n";
+		PhotonNetwork.LobbyStatistics.ForEach(q => list += q.ToString()+"\n");
+		Debug.Log(list);
+	}
+
+
+
+
+}
---
+++ Assets/App/Scripts/player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+	public int union = 0;
+	public int level = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnGUI()
+	{
+		// 何か右端に出すおまじない
+		GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+		GUILayout.BeginHorizontal();
+		{
+			GUILayout.FlexibleSpace();
+
+			GUILayout.BeginVertical();
+			{
+				GUILayout.Label("Union:"+union);
+				GUILayout.Label("Level:"+level);
+			}
+			GUILayout.EndVertical();
+		}
+		GUILayout.EndHorizontal();
+		GUILayout.EndArea();
+
+
+	}
+
+}
