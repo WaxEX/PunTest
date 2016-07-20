00f453b36d7b65a032457de2bbefe04d07aa23e8
Wataru Mizutani <mizutani@success-corp.co.jp>
2016-07-20 20:41:47 +0900

    no message

--- Assets/App/Scripts/PhotonConnector.cs
+++ Assets/App/Scripts/PhotonConnector.cs
@@ -2,148 +2,125 @@
 using System;
 using System.Collections;
 
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+
+
 public class PhotonConnector : Photon.PunBehaviour {
 
 
 	private GameObject player;
 
 
-	// Use this for initialization
+/*********************************************************************
+ * Unity funcs
+ *********************************************************************/
 	void Start () {
-		Debug.Log("Start");
-
 		player = GameObject.Find("Player");
 
-		//PhotonNetwork.ConnectUsingSettings("0.1");
+		Debug.Log("Start");
+
+		PhotonNetwork.ConnectUsingSettings("0.1");
 	}
-
-	// when AutoJoinLobby is off, this method gets called when PUN finished the connection (instead of OnJoinedLobby())
-	public override void OnConnectedToMaster()
-	{
-		Debug.Log("OnConnectedToMaster");
-
-		_loggingLobbyList ();
-
-		string lobby_name = "Union0"+player.GetComponent<player>().union;
-
-		TypedLobby targetLobby = new TypedLobby(lobby_name, LobbyType.Default); 
-		PhotonNetwork.JoinLobby(targetLobby);
-
-
-
-		// コレ使えば楽なんじゃねー？
-		//static bool PhotonNetwork.JoinOrCreateRoom(	string		roomName,
-		//												RoomOptions	roomOptions,
-		//												TypedLobby	typedLobby 
-		//)	
-	}
-
-	public override void OnJoinedLobby()
-	{
-		Debug.Log("OnJoinedLobby");
-
-		_loggingLobbyList ();
-
-		_loggingRoomList();
-
-
-		if(PhotonNetwork.room != null) Debug.Log(PhotonNetwork.room.ToString());
-
-		byte maxMem = 0;
-
-		ExitGames.Client.Photon.Hashtable prop = new ExitGames.Client.Photon.Hashtable();
-		prop["LvZone"] = (int) player.GetComponent<player>().level /10;
-
-		PhotonNetwork.JoinRandomRoom(prop, maxMem);
-	}
-
-
-	public void OnPhotonRandomJoinFailed()
-	{
-		Debug.Log("OnPhotonRandomJoinFailed");
-
-
-		ExitGames.Client.Photon.Hashtable prop = new ExitGames.Client.Photon.Hashtable();
-		prop["LvZone"] = (int) player.GetComponent<player>().level /10;
-
-		RoomOptions option = new RoomOptions();
-		option.maxPlayers = 0;
-		option.CustomRoomProperties = prop;
-
-		PhotonNetwork.CreateRoom(
-			null,
-			option,
-			null
-		);
-	}
-
-	public override void OnJoinedRoom()
-	{
-		Debug.Log("OnJoinedRoom");
-
-		_loggingRoomList ();
-
-		if(PhotonNetwork.room != null) Debug.Log(PhotonNetwork.room.ToString());
-	}
-
-
-
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
 
 	public void OnGUI()
 	{
 		GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString ());
-
-		//GUILayout.Label (PhotonNetwork.lobby.ToString ());
-
-		//if(PhotonNetwork.room != null)GUILayout.Label (PhotonNetwork.room.ToString ());
+		GUILayout.Label (PhotonNetwork.lobby.ToString ());
+		if(PhotonNetwork.room != null) GUILayout.Label (PhotonNetwork.room.ToString ());
 	}
 
 
+/*********************************************************************
+ * PUN
+ *********************************************************************/
+	// when AutoJoinLobby is off, this method gets called when PUN finished the connection (instead of OnJoinedLobby())
+	public override void OnConnectedToMaster(){
+		Debug.Log("OnConnectedToMaster");
+		PhotonNetwork.JoinLobby(getLobbyInfo());
+
+		//static bool PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, typedLobby); //コレ使えば楽なんじゃねー？
+	}
+
+	public override void OnJoinedLobby(){
+		Debug.Log("OnJoinedLobby");
+		_loggingLobbyList();
+
+		StartCoroutine ("waitLog");
+	}
+
+	// コルーチンで処理を遅くする
+	private IEnumerator waitLog() { 
+		yield return new WaitForSeconds (1.0f);  
+		_loggingRoomList();
+	} 
+		
 	// ボタンアタッチとかで起動。ROOMに入る
-//	public void JoinRoom(){
+	public void JoinRoom(){
+		Debug.Log("tryJoinRoom");
 
-//		Debug.Log("tryJoinRoom");
+		// 部屋に入ってる場合
+		if (PhotonNetwork.room != null) {
+			PhotonNetwork.LeaveRoom();
+			return;
+		}
 
-	//	byte maxMem = 0;
-	
-	//	ExitGames.Client.Photon.Hashtable prop = new ExitGames.Client.Photon.Hashtable();
-	//	prop["LvZone"] = (int) player.GetComponent<player>().level /10;
+		PhotonNetwork.JoinRandomRoom(getRoomProperties(), 0);
+	}
+		
 
-	//	PhotonNetwork.JoinRandomRoom(prop, maxMem);
-//	}
+	public void OnPhotonRandomJoinFailed(){
+		Debug.Log("OnPhotonRandomJoinFailed");
+		PhotonNetwork.CreateRoom(null, getRoomOptions(), null);
+	}
+		
+	public override void OnJoinedRoom(){
+		Debug.Log("OnJoinedRoom");
+	}
 
+	private TypedLobby getLobbyInfo(){
+		string lobby_name = "Union0"+player.GetComponent<player>().union;
+		return new TypedLobby(lobby_name, LobbyType.Default);
+	}
 
+	private Hashtable getRoomProperties(){
+		return new Hashtable(){
+			{"LvZone", (int)player.GetComponent<player>().level /10}
+		};
+	}
 
+	private RoomOptions getRoomOptions(){
+		RoomOptions option = new RoomOptions();
+		option.IsVisible  = true;
+		option.IsOpen     = true;
+		option.maxPlayers = 3;
 
-// for DEBUG
+		option.CustomRoomProperties = getRoomProperties();
+		option.customRoomPropertiesForLobby = new string[] {"LvZone"};
+
+		return option;
+	}
+		
+
+/*********************************************************************
+ * for DEBUG
+ *********************************************************************/
 	private void _loggingLobbyList(){
 		var list = "===Lobbies: ";
 		list += PhotonNetwork.LobbyStatistics.Count+"===\n";
 		PhotonNetwork.LobbyStatistics.ForEach(q => list += q.ToString()+"\n");
-		Debug.Log(list);
+		Debug.LogError(list);
 	}
 
-
-
-	private void _loggingRoomList(){
+	public void _loggingRoomList(){
 		if(!PhotonNetwork.insideLobby){
 			Debug.Log("You're out of lobby...");
 			return;
 		}
 
 		var rooms = PhotonNetwork.GetRoomList();
-
 		var list = "===Rooms: ";
 		list += rooms.Length+"===\n";
-		//rooms.ForEach(q => list += q.ToString()+"\n");
-		Debug.Log(list);
+		Array.ForEach(rooms, q => list += q.ToString()+"\n");
+		Debug.LogError(list);
 	}
-
-
 }
